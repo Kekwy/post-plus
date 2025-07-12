bb41b7a0240988339d2743b3ab50f1bfa6e16f95
Kekwy <keekkewy@qq.com>
2025-07-12 21:30:23 +0800

    feat: add editor template

--- frontend/src/pages/EditorPage.tsx
+++ frontend/src/pages/EditorPage.tsx
@@ -1,18 +1,22 @@
-import { EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+// import { EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react'
+// import StarterKit from '@tiptap/starter-kit'
 
-// define your extension array
-const extensions = [StarterKit]
+// // define your extension array
+// const extensions = [StarterKit]
 
-const content = '<p>Hello World!</p>'
+// const content = '<p>Hello World!</p>'
+
+
+import {SimpleEditor} from "../../@/components/tiptap-templates/simple/simple-editor";
 
 const EditorPage = () => {
-  return (
-    <EditorProvider extensions={extensions} content={content}>
-      <FloatingMenu editor={null}>This is the floating menu</FloatingMenu>
-      <BubbleMenu editor={null}>This is the bubble menu</BubbleMenu>
-    </EditorProvider>
-  )
+  // return (
+  //   <EditorProvider extensions={extensions} content={content}>
+  //     <FloatingMenu editor={null}>This is the floating menu</FloatingMenu>
+  //     <BubbleMenu editor={null}>This is the bubble menu</BubbleMenu>
+  //   </EditorProvider>
+  // )
+  return <SimpleEditor />
 }
 
 export default EditorPage;
