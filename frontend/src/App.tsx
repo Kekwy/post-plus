8a50aa978be0516303f181b50551f263dd9d3396
Kekwy <keekkewy@qq.com>
2025-07-12 20:23:24 +0800

    feat: add layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,77 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, ConfigProvider} from "antd";
+import {createStyles} from 'antd-style';
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
+import HomePage from "./page/Home";
+
+const useStyle = createStyles(({prefixCls, css}) => ({
+  linearGradientButton: css`
+      &.${prefixCls}-btn-primary:not([disabled]):not(.${prefixCls}-btn-dangerous) {
+          > span {
+              position: relative;
+          }
+
+          &::before {
+              content: '';
+              background: linear-gradient(135deg, #6253e1, #04befe);
+              position: absolute;
+              inset: -1px;
+              opacity: 1;
+              transition: all 0.3s;
+              border-radius: inherit;
+          }
+
+          &:hover::before {
+              opacity: 0;
+          }
+      }
+  `,
+}));
 
 function App() {
+  const {styles} = useStyle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ConfigProvider
+      button={{
+        className: styles.linearGradientButton,
+      }}
+    >
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* 首页路由 */}
+            <Route
+              path="/"
+              element={
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo"/>
+                  <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                  <Link to="/home">
+                    <Button type="primary">进入首页</Button>
+                  </Link>
+                </header>
+              }
+            />
+            {/* 新页面路由 */}
+            <Route path="/home" element={<HomePage/>}/>
+          </Routes>
+        </div>
+      </Router>
+    </ConfigProvider>
   );
 }
 
